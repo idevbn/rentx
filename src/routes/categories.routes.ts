e23003cfaded687eb79d0644a2bec59df043fc7d
Idevaldo <inetomec@yahoo.com.br>
2022-05-11 11:24:09 -0300

    feat: :sparkles: Creating Category Use Cases

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,13 @@
 import { CategoriesRepository } from "@modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "@modules/cars/services/CreateCategoryService";
-import { Request, Response, Router } from "express";
+import { createCategoryController } from "@modules/cars/useCases/createCategory";
+import { Router } from "express";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (request: Request, response: Response) => {
-  const { name, description } = request.body;
+categoriesRoutes.post("/", createCategoryController.handle);
 
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
-});
-
-categoriesRoutes.get("/", (request: Request, response: Response) => {
+categoriesRoutes.get("/", (request, response) => {
   const categoriesList = categoriesRepository.list();
 
   return response.json(categoriesList);
