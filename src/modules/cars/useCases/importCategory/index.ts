f58a7784bce3aaa022c04c832eb4f57f911d565b
Idevaldo <inetomec@yahoo.com.br>
2022-05-11 19:43:49 -0300

    feat: :sparkles: Reading Upload Data

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,8 +1,10 @@
+import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
 
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(
   importCategoryUseCase
 );
