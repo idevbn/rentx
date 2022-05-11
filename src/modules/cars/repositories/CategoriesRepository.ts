cbb04a19eff668b39bfd9de2a8551e52becb1bdd
Idevaldo <inetomec@yahoo.com.br>
2022-05-11 12:13:54 -0300

    refactor: :sparkles: Adding Missing Implementation to Categories Repository

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -1,8 +1,11 @@
 import { Category } from "@modules/cars/model/Category";
 
-import { ICreateCategoryDTO } from "./ICategoriesRepository";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[] = [];
 
   constructor() {
