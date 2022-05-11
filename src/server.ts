e312a72fc3147e87cc0aa58bbfaa0f1fcf050edd
Idevaldo <inetomec@yahoo.com.br>
2022-05-11 14:05:04 -0300

    feat: :sparkles: Refactoring Routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import express from "express";
-
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "routes";
 
 const app = express();
 
@@ -10,12 +8,7 @@ const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
-});
-
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => {
   console.log(
