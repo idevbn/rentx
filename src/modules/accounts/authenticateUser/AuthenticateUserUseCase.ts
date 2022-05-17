ab0c221d137600184f8548e77442019a2c43d933
Idevaldo <inetomec@yahoo.com.br>
2022-05-17 10:58:43 -0300

    feat: :sparkles: Creating User Token

---
+++ src/modules/accounts/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,51 @@
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign({}, "930d7c00436b350e56a2d39fa779b142", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import swaggerFile from "./swagger.json";
 
 import "./database";
 
-import "./shared/container";
+import "@shared/container";
 
 const app = express();
 
