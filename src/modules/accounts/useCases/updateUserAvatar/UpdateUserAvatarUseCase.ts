38ea8f6789f84498d23db2069d42e86423e3d397
Idevaldo <inetomec@yahoo.com.br>
2022-05-19 10:30:12 -0300

    feat: :sparkles: Adding Avatar Column

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,25 @@
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { inject, injectable } from "tsyringe";
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  public async execute({ avatar_file, user_id }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+
+    user.avatar = avatar_file;
+
+    await this.usersRepository.create(user);
+  }
+}
+
+export { UpdateUserAvatarUseCase };
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { Router } from "express";
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.patch("/avatar", updateUserAvatarController.handle);
 
 export { usersRoutes };
