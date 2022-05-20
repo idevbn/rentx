1c303e85e6d2f05019c7d1a95278bd7a6ca367f4
Idevaldo <inetomec@yahoo.com.br>
2022-05-20 08:49:12 -0300

    test: :test_tube: Creating Our First Test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Create category", () => {
+  it("Hoping that 2 plus 2 is equal to 4", () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it("Hoping that 2 plus 2 is not equal to 5", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
