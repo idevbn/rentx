ef506a586a4c5e18cdf0b68b44c5c00162f764c0
Idevaldo <inetomec@yahoo.com.br>
2022-07-13 22:37:21 -0300

    wip: :construction: Supertest Config in Progress

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,43 +1,7 @@
-import "reflect-metadata";
-import express, { Request, Response, NextFunction } from "express";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
-
-import { AppError } from "@shared/errors/AppError";
-import { router } from "@shared/infra/http/routes";
-import createConnection from "@shared/infra/typeorm";
-
-import swaggerFile from "../../../swagger.json";
-
-import "@shared/container";
-
-createConnection();
-
-const app = express();
+import { app } from "./app";
 
 const url = "http://localhost:";
 const PORT = process.env.PORT || 3333;
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error ${err.message}`,
-    });
-  }
-);
 
 app.listen(3333, () => {
   console.log(
