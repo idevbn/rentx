027b4f2f937fb44f68e32806e62db8700a0cde74
Idevaldo <inetomec@yahoo.com.br>
2022-07-17 19:57:21 -0300

    wip: :construction: Executing Integration Tests

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,7 +1,40 @@
+import { hash } from "bcryptjs";
 import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuid } from "uuid";
 
 import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm/";
+
+let connection: Connection;
 
 describe("Create Category Controller", async () => {
-  await request(app).get("/cars/available").expect(200);
+  beforeEach(async () => {
+    connection = await createConnection();
+
+    const id = uuid();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
+    `
+    );
+  });
+
+  it("Should be ablet o create a new category", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    console.log(responseToken);
+
+    const response = await request(app).post("/categories").send({
+      name: "Category Supertest",
+      description: "Category Supertest",
+    });
+
+    expect(response.status).toBe(201);
+  });
 });
