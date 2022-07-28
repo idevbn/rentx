1169a7d4946300310e8ba608a52f106af77ee554
Idevaldo <inetomec@yahoo.com.br>
2022-07-27 22:32:41 -0300

    feat: :sparkles: Listing Rentals by User

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -3,6 +3,7 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
 
 interface IRentalsRepository {
+  findAllUsersById(user_id: string): Promise<Rental[]>;
   findById(id: string): Promise<Rental>;
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
