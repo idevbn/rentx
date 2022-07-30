55ef2ec4320d48f2a003b7c9bb0254ebb277fc19
Idevaldo <inetomec@yahoo.com.br>
2022-07-30 17:07:30 -0300

    test: :test_tube: Fixing tests

--- src/modules/cars/useCases/createCars/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCars/CreateCarUseCase.spec.ts
@@ -27,43 +27,41 @@ describe("Create Car", () => {
     expect(car).toHaveProperty("id");
   });
 
-  it("Should not be able to create a car with same license plate", () => {
-    expect(async () => {
-      await createCarUseCase.execute({
-        name: "Name Car",
-        description: "Description Car",
-        daily_rate: 100,
-        license_plate: "ABC-1234",
-        fine_amount: 60,
-        brand: "Brand",
-        category_id: "category",
-      });
-
-      expect(async () => {
-        await createCarUseCase.execute({
-          name: "Name Car 2",
-          description: "Description Car 2",
-          daily_rate: 100,
-          license_plate: "ABC-1234",
-          fine_amount: 60,
-          brand: "Brand 2",
-          category_id: "category 2",
-        });
-      }).rejects.toBeInstanceOf(AppError);
-    });
-  });
-
-  it("Should be able to create a car with available true by default", async () => {
-    const car = await createCarUseCase.execute({
-      name: "Car Available",
-      description: "Description Car Available",
+  it("Should not be able to create a car with same license plate", async () => {
+    await createCarUseCase.execute({
+      name: "Name Car",
+      description: "Description Car",
       daily_rate: 100,
-      license_plate: "ABCD-1234",
+      license_plate: "ABC-1234",
       fine_amount: 60,
       brand: "Brand",
       category_id: "category",
     });
 
-    expect(car.available).toBe(true);
+    await expect(
+      createCarUseCase.execute({
+        name: "Name Car 2",
+        description: "Description Car 2",
+        daily_rate: 100,
+        license_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "Brand 2",
+        category_id: "category 2",
+      })
+    ).rejects.toEqual(new AppError("Car already exists!"));
   });
+});
+
+it("Should be able to create a car with available true by default", async () => {
+  const car = await createCarUseCase.execute({
+    name: "Car Available",
+    description: "Description Car Available",
+    daily_rate: 100,
+    license_plate: "ABCD-1234",
+    fine_amount: 60,
+    brand: "Brand",
+    category_id: "category",
+  });
+
+  expect(car.available).toBe(true);
 });
--- src/modules/rentals/repositories/inMemory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/inMemory/RentalsRepositoryInMemory.ts
@@ -36,6 +36,14 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
 
     return rental;
   }
+
+  async findAllUsersById(user_id: string): Promise<Rental[]> {
+    return this.rentals.filter((rental) => rental.user_id === user_id);
+  }
+
+  async findById(id: string): Promise<Rental> {
+    return this.rentals.find((rental) => rental.id === id);
+  }
 }
 
 export { RentalsRepositoryInMemory };
